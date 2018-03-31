821e52a7f42cecb0bac1e88b1eee0a85f4aacd4b
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-31 00:03:49 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,6 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db, :hp
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, db:, hp:)
     @id = id
     @name = name
     @type = type
@@ -13,7 +13,7 @@ class Pokemon
 
   def self.find(id,db)
     result = db.execute("SELECT * FROM pokemon where id = ?",id)
-    Pokemon.new(id: result[0][0], name: result[0][1], type: result[0][2],db: db)
+    Pokemon.new(id: result[0][0], name: result[0][1], type: result[0][2],db: db, )
   end
 
   def alter_hp(hp,db)
